e2db6eb399e2f40505d7a74361b24fdc4f1185bd
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-23 00:53:15 -0300

    feat: adiciona manipulador de erros para tratamento de erros do Fastify

---
+++ src/config/erro/ErrorHandler.ts
@@ -0,0 +1,30 @@
+import { FastifyError, FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { AppError } from "./AppError";
+
+export function errorHandler(
+  error: FastifyError,
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  if (error instanceof AppError) {
+    reply.status(error.statusCode).send({
+      status: "error",
+      message: error.message,
+    });
+    return;
+  }
+
+  if (error instanceof z.ZodError) {
+    reply.status(400).send({
+      status: "error",
+      message: error.errors,
+    });
+    return;
+  }
+
+  reply.status(500).send({
+    status: "error",
+    message: `Internal server error - ${error.message || "Unexpected error"}`,
+  });
+}

4db9788165bceb58dc645f4d4b6afb5c5b20e7c0
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-23 00:53:39 -0300

    feat: adiciona interface IUserRepository com métodos para criação e busca de usuários

---
+++ src/modules/user/repositories/interface/IUserRepository.ts
@@ -0,0 +1,9 @@
+import { User } from "@prisma/client";
+import { UserDTOType } from "../../dtos/UserDTO";
+
+interface IUserRepository {
+    create(data: UserDTOType): Promise<User>;
+    findByEmail(email: string): Promise<User | null>;
+}
+
+export { IUserRepository };

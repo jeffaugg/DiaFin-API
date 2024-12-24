30378dbab6ba958141a2dfcca9dd22a99d3fdc13
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-24 16:48:54 -0300

    feat: adiciona rota de login e rota para obter informações do usuário autenticado

--- src/shared/http/routes/api/userRoutes.ts
+++ src/shared/http/routes/api/userRoutes.ts
@@ -1,10 +1,20 @@
 import { FastifyInstance } from "fastify";
 import { CreateUserFactory } from "../../../../modules/user/factories/CreateUserFactory";
+import { LoginUserFactory } from "../../../../modules/user/factories/LoginUserFactory";
+import { isAuthenticate } from "../../middlewares/isAuthenticate";
 
 const createUserController = CreateUserFactory.create();
-
+const loginUserController = LoginUserFactory.create();
 export async function userRoutes(app: FastifyInstance) {
-    app.post("/signup", async (req, reply) => {
-        return createUserController.handle(req, reply);
-    });
+  app.post("/signup", async (req, reply) => {
+    return createUserController.handle(req, reply);
+  });
+
+  app.post("/signin", async (req, reply) => {
+    return loginUserController.handle(req, reply);
+  });
+
+  app.get("/me", { preHandler: [isAuthenticate] }, async (req, reply) => {
+    return reply.send(req.user);
+  });
 }

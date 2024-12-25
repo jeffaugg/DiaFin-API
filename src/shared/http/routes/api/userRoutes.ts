8743f3a51d9d2b6cb47b8a1389dc70ffab0224d0
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-25 18:48:33 -0300

    feat: adiciona rotas para criação e exibição de perfil financeiro de usuário

--- src/shared/http/routes/api/userRoutes.ts
+++ src/shared/http/routes/api/userRoutes.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance } from "fastify";
-import { CreateUserFactory } from "../../../../modules/user/factories/CreateUserFactory";
 import { LoginUserFactory } from "../../../../modules/user/factories/LoginUserFactory";
 import { isAuthenticate } from "../../middlewares/isAuthenticate";
+import { CreateUserFactory } from "../../../../modules/user/factories/CreateUserFactory";
+import { CreateFinancialProfileFactory } from "../../../../modules/user/factories/CreateFinancialProfileFactory";
+import { ShowUserFactory } from "../../../../modules/user/factories/ShowUserFactory";
 
 const createUserController = CreateUserFactory.create();
 const loginUserController = LoginUserFactory.create();
+const profileUserController = CreateFinancialProfileFactory.create();
+const showUserController = ShowUserFactory.create();
 export async function userRoutes(app: FastifyInstance) {
   app.post("/signup", async (req, reply) => {
     return createUserController.handle(req, reply);
@@ -15,6 +19,10 @@ export async function userRoutes(app: FastifyInstance) {
   });
 
   app.get("/me", { preHandler: [isAuthenticate] }, async (req, reply) => {
-    return reply.send(req.user);
+    return showUserController.handle(req, reply);
+  });
+
+  app.post("/profile", { preHandler: [isAuthenticate] }, async (req, reply) => {
+    return profileUserController.handle(req, reply);
   });
 }

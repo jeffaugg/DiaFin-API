7998006f8403758c62e4ec21df40858d57cfd414
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-25 18:45:59 -0300

    feat: adiciona método para busca de usuário por ID com perfil financeiro

--- src/modules/user/repositories/interface/IUserRepository.ts
+++ src/modules/user/repositories/interface/IUserRepository.ts
@@ -1,8 +1,13 @@
-import { User } from "@prisma/client";
+import { FinancialProfile, User } from "@prisma/client";
 import { UserDTOType } from "../../dtos/UserDTO";
+
+export type UserFull = User & {
+  financialProfile: FinancialProfile | null;
+};
 interface IUserRepository {
   create(data: UserDTOType): Promise<User>;
   findByEmail(email: string): Promise<User | null>;
+  findById(id: number): Promise<UserFull | null>;
 }
 
 export { IUserRepository };

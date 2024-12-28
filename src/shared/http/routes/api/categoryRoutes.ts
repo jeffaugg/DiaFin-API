a242fe3fb9afb77c9ec6856c28077fc64f2868d8
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-28 17:06:08 -0300

    feat: adiciona rotas para exibição, busca e exclusão de categorias

--- src/shared/http/routes/api/categoryRoutes.ts
+++ src/shared/http/routes/api/categoryRoutes.ts
@@ -1,10 +1,28 @@
 import { FastifyInstance } from "fastify";
 import { CreateCategoryFactory } from "../../../../modules/expense/factories/CreateCategoryFactory";
 import { isAuthenticate } from "../../middlewares/isAuthenticate";
+import { ShowCategoriesFactory } from "../../../../modules/expense/factories/ShowCategoriesFactory";
+import { FindCategoryByIdFactory } from "../../../../modules/expense/factories/FindCategoryByIdFactory";
+import { DeleteCategoryFactory } from "../../../../modules/expense/factories/DeleteCategoryFactory";
 
 const createCategoryController = CreateCategoryFactory.create();
+const showCategoriesController = ShowCategoriesFactory.create();
+const findCategoryByIdController = FindCategoryByIdFactory.create();
+const deleteCategoryController = DeleteCategoryFactory.create();
 export async function categoryRoutes(app: FastifyInstance) {
   app.post("/", { preHandler: [isAuthenticate] }, async (req, reply) => {
     return createCategoryController.handle(req, reply);
+  });
+
+  app.get("/", { preHandler: [isAuthenticate] }, async (req, reply) => {
+    return showCategoriesController.handle(req, reply);
+  });
+
+  app.get("/:id", { preHandler: [isAuthenticate] }, async (req, reply) => {
+    return findCategoryByIdController.handle(req, reply);
+  });
+
+  app.delete("/:id", { preHandler: [isAuthenticate] }, async (req, reply) => {
+    return deleteCategoryController.handle(req, reply);
   });
 }

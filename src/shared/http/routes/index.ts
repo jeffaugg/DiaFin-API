dfc81e85ee260c3e45ece01f37fdb65ea353db6f
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-27 21:42:28 -0300

    feat: adiciona rotas para gerenciamento de categorias

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { userRoutes } from "./api/userRoutes";
+import { categoryRoutes } from "./api/categoryRoutes";
 
 export async function Routes(app: FastifyInstance) {
   app.register(userRoutes, { prefix: "/user" });
+  app.register(categoryRoutes, { prefix: "/category" });
 }

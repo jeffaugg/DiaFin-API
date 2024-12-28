71efeaaf10aa376da8629ce695c6e0a486dc4152
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-27 21:42:24 -0300

    feat: adiciona caso de uso para criação de categorias

---
+++ src/modules/expense/useCases/createCategory.ts/CreateCategoryUseCase.ts
@@ -0,0 +1,24 @@
+import { Category } from "@prisma/client";
+import { ICategoryRepository } from "../../repositories/interface/ICategoryRepository";
+import { AppError } from "../../../../config/erro/AppError";
+import { CategoryDTOType } from "../../dtos/CategoryDTO";
+
+export class CreateCategoryUseCase {
+  constructor(private categoryRepository: ICategoryRepository) {}
+
+  async execute(data: CategoryDTOType, userId: number): Promise<Category> {
+    const categoryAlreadyExists = await this.categoryRepository.findByName(
+      data.name,
+    );
+
+    if (categoryAlreadyExists) {
+      throw new AppError("Category already exists", 400);
+    }
+
+    const categoryData = { ...data, userId };
+
+    const category = await this.categoryRepository.create(categoryData);
+
+    return category;
+  }
+}

37ac750e4b1bcd87c211e54e7acca14f876a834d
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-29 12:57:25 -0300

    feat: adiciona suporte a CORS e documentação Swagger com Fastify

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,32 @@ import fastify from "fastify";
 import { prisma } from "./shared/prisma/PrismaService";
 import { errorHandler } from "./config/erro/ErrorHandler";
 import { Routes } from "./shared/http/routes";
+import { fastifyCors } from "@fastify/cors";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
 const app = fastify();
 
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+app.register(fastifyCors, { origin: "*" });
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "DIAFIN API",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 app.addHook("onClose", async (instance) => {
   await prisma.$disconnect();
 });

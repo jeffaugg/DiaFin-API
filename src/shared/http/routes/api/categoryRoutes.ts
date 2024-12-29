958b5bf1f70f5fbd36afaba8333b6861734d78ab
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-29 12:57:53 -0300

    feat: adiciona validação de esquema e documentação para rotas de categorias

--- src/shared/http/routes/api/categoryRoutes.ts
+++ src/shared/http/routes/api/categoryRoutes.ts
@@ -4,25 +4,94 @@ import { isAuthenticate } from "../../middlewares/isAuthenticate";
 import { ShowCategoriesFactory } from "../../../../modules/expense/factories/ShowCategoriesFactory";
 import { FindCategoryByIdFactory } from "../../../../modules/expense/factories/FindCategoryByIdFactory";
 import { DeleteCategoryFactory } from "../../../../modules/expense/factories/DeleteCategoryFactory";
+import { CategoryDTO } from "../../../../modules/expense/dtos/CategoryDTO";
+import z from "zod";
 
 const createCategoryController = CreateCategoryFactory.create();
 const showCategoriesController = ShowCategoriesFactory.create();
 const findCategoryByIdController = FindCategoryByIdFactory.create();
 const deleteCategoryController = DeleteCategoryFactory.create();
 export async function categoryRoutes(app: FastifyInstance) {
-  app.post("/", { preHandler: [isAuthenticate] }, async (req, reply) => {
-    return createCategoryController.handle(req, reply);
-  });
+  app.post(
+    "/",
+    {
+      schema: {
+        tags: ["Category"],
+        description: "Create category",
+        body: CategoryDTO.omit({ userId: true }),
+        response: {
+          201: CategoryDTO,
+          400: z.object({
+            statusCode: z.literal(400),
+            message: z.literal("Category already exists"),
+          }),
+        },
+      },
+      preHandler: [isAuthenticate],
+    },
+    async (req, reply) => {
+      return createCategoryController.handle(req, reply);
+    },
+  );
 
-  app.get("/", { preHandler: [isAuthenticate] }, async (req, reply) => {
-    return showCategoriesController.handle(req, reply);
-  });
+  app.get(
+    "/",
+    {
+      schema: {
+        tags: ["Category"],
+        description: "List all categories",
+        response: {
+          200: z.array(CategoryDTO),
+        },
+      },
+      preHandler: [isAuthenticate],
+    },
+    async (req, reply) => {
+      return showCategoriesController.handle(req, reply);
+    },
+  );
 
-  app.get("/:id", { preHandler: [isAuthenticate] }, async (req, reply) => {
-    return findCategoryByIdController.handle(req, reply);
-  });
+  app.get(
+    "/:id",
+    {
+      schema: {
+        tags: ["Category"],
+        description: "Find category by id",
+        response: {
+          200: CategoryDTO,
+          404: z
+            .object({
+              statusCode: z.literal(404),
+              message: z.literal("Category not found"),
+            })
+            .describe("Category not found"),
+        },
+      },
+      preHandler: [isAuthenticate],
+    },
+    async (req, reply) => {
+      return findCategoryByIdController.handle(req, reply);
+    },
+  );
 
-  app.delete("/:id", { preHandler: [isAuthenticate] }, async (req, reply) => {
-    return deleteCategoryController.handle(req, reply);
-  });
+  app.delete(
+    "/:id",
+    {
+      schema: {
+        tags: ["Category"],
+        description: "Delete category",
+        response: {
+          200: z.object({ message: z.literal("Category deleted") }),
+          404: z.object({
+            statusCode: z.literal(404),
+            message: z.literal("Category not found"),
+          }),
+        },
+      },
+      preHandler: [isAuthenticate],
+    },
+    async (req, reply) => {
+      return deleteCategoryController.handle(req, reply);
+    },
+  );
 }

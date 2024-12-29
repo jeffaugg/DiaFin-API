05b189341654312e26353bce3ee456a107d97278
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-29 12:57:58 -0300

    feat: adiciona validação de esquema e documentação para rotas de usuário

--- src/shared/http/routes/api/userRoutes.ts
+++ src/shared/http/routes/api/userRoutes.ts
@@ -4,25 +4,102 @@ import { isAuthenticate } from "../../middlewares/isAuthenticate";
 import { CreateUserFactory } from "../../../../modules/user/factories/CreateUserFactory";
 import { CreateFinancialProfileFactory } from "../../../../modules/user/factories/CreateFinancialProfileFactory";
 import { ShowUserFactory } from "../../../../modules/user/factories/ShowUserFactory";
+import { UserDTO } from "../../../../modules/user/dtos/UserDTO";
+import z from "zod";
+import { LoginDTO } from "../../../../modules/user/useCases/loginUser/LoginDTO";
+import { FinancialProfileDTO } from "../../../../modules/user/dtos/FinancialProfileDTO";
 
 const createUserController = CreateUserFactory.create();
 const loginUserController = LoginUserFactory.create();
 const profileUserController = CreateFinancialProfileFactory.create();
 const showUserController = ShowUserFactory.create();
 export async function userRoutes(app: FastifyInstance) {
-  app.post("/signup", async (req, reply) => {
-    return createUserController.handle(req, reply);
-  });
+  app.post(
+    "/signup",
+    {
+      schema: {
+        tags: ["User"],
+        description: "Create user",
+        body: UserDTO,
+        response: {
+          201: UserDTO,
+          400: z.object({
+            statusCode: z.literal(409),
+            message: z.literal("User already exists"),
+          }),
+        },
+      },
+    },
+    async (req, reply) => {
+      return createUserController.handle(req, reply);
+    },
+  );
 
-  app.post("/signin", async (req, reply) => {
-    return loginUserController.handle(req, reply);
-  });
+  app.post(
+    "/signin",
+    {
+      schema: {
+        tags: ["User"],
+        description: "Login user",
+        body: LoginDTO,
+        response: {
+          200: z.object({
+            token: z.string(),
+          }),
+          400: z.object({
+            statusCode: z.literal(401),
+            message: z.literal("Username or password does not exist"),
+          }),
+        },
+      },
+    },
+    async (req, reply) => {
+      return loginUserController.handle(req, reply);
+    },
+  );
 
-  app.get("/me", { preHandler: [isAuthenticate] }, async (req, reply) => {
-    return showUserController.handle(req, reply);
-  });
+  app.get(
+    "/me",
+    {
+      schema: {
+        tags: ["User"],
+        description: "Show user",
+        response: {
+          200: UserDTO.extend({
+            financialProfile: FinancialProfileDTO,
+          }),
+          404: z.object({
+            statusCode: z.literal(404),
+            message: z.literal("User not found"),
+          }),
+        },
+      },
+      preHandler: [isAuthenticate],
+    },
+    async (req, reply) => {
+      return showUserController.handle(req, reply);
+    },
+  );
 
-  app.post("/profile", { preHandler: [isAuthenticate] }, async (req, reply) => {
-    return profileUserController.handle(req, reply);
-  });
+  app.post(
+    "/profile",
+    {
+      schema: {
+        tags: ["User"],
+        description: "Create financial profile",
+        body: FinancialProfileDTO,
+        response: {
+          201: FinancialProfileDTO,
+          400: z.object({
+            statusCode: z.literal(409),
+            message: z.literal("Financial profile already exists"),
+          }),
+        },
+      },
+      preHandler: [isAuthenticate],
+    },
+    async (req, reply) => {
+      return profileUserController.handle(req, reply);
+    },
+  );
 }

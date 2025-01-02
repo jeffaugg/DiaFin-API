a05c8700ab794e1e23869056fb7ec3777e29d53e
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-01 22:57:05 -0300

    feat: adiciona rotas de despesas ao aplicativo

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { userRoutes } from "./api/userRoutes";
 import { categoryRoutes } from "./api/categoryRoutes";
+import { expenseRoutes } from "./api/expenseRoutes";
 
 export async function Routes(app: FastifyInstance) {
   app.register(userRoutes, { prefix: "/user" });
   app.register(categoryRoutes, { prefix: "/category" });
+  app.register(expenseRoutes, { prefix: "/expense" });
 }

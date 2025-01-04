ccc2fd70ca2ebea3e9c10b4d2e9072dcb2d24114
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-04 20:45:18 -0300

    feat: adiciona DTO de resposta para despesas com validações

---
+++ src/modules/expense/dtos/ExpenseDTO.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const ExpenseDTO = z.object({
+  value: z.number().int().positive({ message: "Value is required" }),
+  description: z.string().min(3, { message: "Description is required" }),
+  categoryId: z.number().int().positive({ message: "Category is required" }),
+});
+
+export const expenseResponse = ExpenseDTO.extend({
+  id: z.number().int().positive(),
+  date: z.string(),
+  userId: z.number().int().positive(),
+  dailyBudgetId: z.number().int().positive(),
+});
+
+export type ExpenseDTOType = z.infer<typeof ExpenseDTO>;

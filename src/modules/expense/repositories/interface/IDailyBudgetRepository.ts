4594ef496cda02309bb49e8c38341535a6956bde
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-04 20:46:01 -0300

    feat: adiciona método de atualização ao repositório DailyBudgetRepository

--- src/modules/expense/repositories/interface/IDailyBudgetRepository.ts
+++ src/modules/expense/repositories/interface/IDailyBudgetRepository.ts
@@ -4,6 +4,7 @@ import { DailyBudgetDTOType } from "../../dtos/DailyBudgetDTO";
 interface IDailyBudgetRepository {
   create(data: DailyBudgetDTOType): Promise<DailyBudget>;
   existsToday(userId: number): Promise<DailyBudget | null>;
+  update(dailyBudget: DailyBudget): Promise<DailyBudget>;
   findById(id: number, userId: number): Promise<DailyBudget | null>;
   list(userId: number): Promise<DailyBudget[]>;
   delete(id: number, userId: number): Promise<void>;

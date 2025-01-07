d8cfc1e5960cab8d5ec960830ba922605aaa6d40
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-06 21:06:14 -0300

    feat: adiciona método de atualização no repositório de despesas

--- src/modules/expense/repositories/interface/IExpenseRepository.ts
+++ src/modules/expense/repositories/interface/IExpenseRepository.ts
@@ -8,6 +8,7 @@ interface IExpenseRepository {
       dailyBudgetId: number;
     },
   ): Promise<Expense>;
+  update(id: number, userId: number, data: ExpenseDTOType): Promise<Expense>;
   findById(id: number, userId: number): Promise<Expense | null>;
   list(userId: number): Promise<Expense[]>;
   delete(id: number, userId: number): Promise<void>;

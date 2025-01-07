50d6c8159671688395447f73fccf21ecd72f468c
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-06 21:03:09 -0300

    feat: adiciona rotas para listar e atualizar despesas

--- src/shared/http/routes/api/expenseRoutes.ts
+++ src/shared/http/routes/api/expenseRoutes.ts
@@ -6,8 +6,12 @@ import {
 import z from "zod";
 import { isAuthenticate } from "../../middlewares/isAuthenticate";
 import { CreateExpenseFactory } from "../../../../modules/expense/factories/CreateExpenseFactory";
+import { ShowExpensesFactory } from "../../../../modules/expense/factories/ShowExpensesFactory";
+import { UpdateExpenseFactory } from "../../../../modules/expense/factories/UpdateExpenseFactory";
 
 const createExpenseController = CreateExpenseFactory.create();
+const showExpensesFactory = ShowExpensesFactory.create();
+const updateExpenseController = UpdateExpenseFactory.create();
 export async function expenseRoutes(app: FastifyInstance) {
   app.post(
     "/",
@@ -30,4 +34,46 @@ export async function expenseRoutes(app: FastifyInstance) {
       return createExpenseController.handle(req, reply);
     },
   );
+
+  app.get("/", {
+    schema: {
+      tags: ["Expense"],
+      description: "List expenses",
+      response: {
+        200: z.array(expenseResponse),
+      },
+    },
+    preHandler: [isAuthenticate],
+    handler: async (req, reply) => {
+      return showExpensesFactory.handle(req, reply);
+    },
+  });
+
+  app.put("/:id", {
+    schema: {
+      tags: ["Expense"],
+      description: "Update expense",
+      params: z.object({
+        id: z.string(),
+      }),
+      body: ExpenseDTO,
+      response: {
+        200: expenseResponse,
+        404: z
+          .object({
+            status: z.string(),
+            message: z.union([
+              z.literal("Expense not found"),
+              z.literal("Category not found"),
+              z.literal("User not found"),
+            ]),
+          })
+          .describe("expense || category || user not found"),
+      },
+    },
+    preHandler: [isAuthenticate],
+    handler: async (req, reply) => {
+      return updateExpenseController.handle(req, reply);
+    },
+  });
 }

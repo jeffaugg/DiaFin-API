154f5badcb6ea093941ff96e3e4eb52884769587
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-10 00:30:20 -0300

    feat: adiciona fábrica para o controlador de exclusão de despesas

---
+++ src/modules/expense/factories/DeleteExpenseFactory.ts
@@ -0,0 +1,28 @@
+import { UserRepository } from "../../user/repositories/UserRepository";
+import { ExpenseRepository } from "../repositories/ExpenseRepository";
+import { UserBalanceService } from "../services/UserBalanceService";
+import { DeleteExpenseUseCase } from "../useCases/deleteExpense/DeleteExpenseUseCase";
+import { DailyBudgetValueService } from "../services/DailyBudgetValueService";
+import { DailyBudgetRepository } from "../repositories/DailyBudgetRepository";
+import { DeleteExpenseController } from "../useCases/deleteExpense/DeleteExpenseController";
+
+export class DeleteExpenseFactory {
+  static create() {
+    const expenseRepository = new ExpenseRepository();
+    const userRepository = new UserRepository();
+    const userBalanceService = new UserBalanceService(userRepository);
+    const dailyBudgetRepository = new DailyBudgetRepository();
+    const dailyBudgetValueService = new DailyBudgetValueService(
+      dailyBudgetRepository,
+    );
+    const deleteExpenseUseCase = new DeleteExpenseUseCase(
+      expenseRepository,
+      userBalanceService,
+      dailyBudgetValueService,
+    );
+    const deleteExpenseController = new DeleteExpenseController(
+      deleteExpenseUseCase,
+    );
+    return deleteExpenseController;
+  }
+}

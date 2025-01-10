4d48bb2140a25807d0ff76cc61132194b08e8e4a
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-10 00:27:46 -0300

    feat: adiciona serviço para calcular o orçamento diário do usuário

---
+++ src/modules/expense/services/DailyBudgetCalculatorService.ts
@@ -0,0 +1,27 @@
+import { differenceInDays, endOfMonth } from "date-fns";
+import { AppError } from "../../../config/erro/AppError";
+import { UserRepository } from "../../user/repositories/UserRepository";
+
+export class DailyBudgetCalculatorService {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute(userId: number): Promise<number> {
+    const user = await this.userRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado", 404);
+    }
+
+    const today = new Date();
+    const lastDayOfMonth = endOfMonth(today);
+
+    const daysRemaining = Math.max(1, differenceInDays(lastDayOfMonth, today));
+
+    if (user.balance === undefined || user.balance === null) {
+      throw new AppError("O saldo do usuário não está definido", 409);
+    }
+    const dailyValue = user.balance / daysRemaining;
+
+    return dailyValue;
+  }
+}

3f46c7d8f233afa659b6d0ff29527e01187d1045
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-09 23:39:01 -0300

    refactor: simplifica a lógica de ajuste de saldo

--- src/modules/expense/useCases/createExpense/CreateExpenseUseCase.ts
+++ src/modules/expense/useCases/createExpense/CreateExpenseUseCase.ts
@@ -3,23 +3,18 @@ import { IExpenseRepository } from "../../repositories/interface/IExpenseReposit
 import { ExpenseDTOType } from "../../dtos/ExpenseDTO";
 import { AppError } from "../../../../config/erro/AppError";
 import { ICategoryRepository } from "../../repositories/interface/ICategoryRepository";
-import { IUserRepository } from "../../../user/repositories/interface/IUserRepository";
-import { IDailyBudgetRepository } from "../../repositories/interface/IDailyBudgetRepository";
 import { ReturnDailyBudgetUseCase } from "../createDailyBudget/ReturnDailyBudgetUseCase";
+import { UserBalanceService } from "../../services/UserBalanceService";
+import { DailyBudgetValueService } from "../../services/DailyBudgetValueService";
 
 export class CreateExpenseUseCase {
-  private returnDailyBudgetUseCase: ReturnDailyBudgetUseCase;
   constructor(
     private expenseRepository: IExpenseRepository,
     private categoryRepository: ICategoryRepository,
-    private userRepository: IUserRepository,
-    private dailyBudgetRepository: IDailyBudgetRepository,
-  ) {
-    this.returnDailyBudgetUseCase = new ReturnDailyBudgetUseCase(
-      this.dailyBudgetRepository,
-      this.userRepository,
-    );
-  }
+    private userBalanceService: UserBalanceService,
+    private dailyBudgetValueService: DailyBudgetValueService,
+    private returnDailyBudgetUseCase: ReturnDailyBudgetUseCase,
+  ) {}
 
   async execute(data: ExpenseDTOType, userId: number): Promise<Expense> {
     const category = await this.categoryRepository.findById(
@@ -31,27 +26,13 @@ export class CreateExpenseUseCase {
       throw new AppError("Category not found", 404);
     }
 
-    const user = await this.userRepository.findById(userId);
-
-    if (!user) {
-      throw new AppError("User not found", 404);
-    }
-
     const dailyBudget = await this.returnDailyBudgetUseCase.execute(userId);
-
-    if (user.balance < data.value) {
-      throw new AppError("Insufficient balance", 400);
-    }
 
     if (data.value > dailyBudget.value) {
       throw new AppError(`Value greater than daily budget`, 400);
     }
-
-    dailyBudget.value -= data.value;
-    user.balance -= data.value;
-    const { financialProfile, ...simpleUser } = user;
-    this.userRepository.update(simpleUser);
-    this.dailyBudgetRepository.update(dailyBudget);
+    await this.userBalanceService.adjustBalance(userId, -data.value);
+    await this.dailyBudgetValueService.adjustValue(userId, -data.value);
 
     const expense = await this.expenseRepository.create({
       ...data,

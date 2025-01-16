01e57c20d88064167f036070abfec6e648845a96
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-15 23:37:12 -0300

    feat: cria interface IIncomeRepository para operações de receitas

---
+++ src/modules/income/repositories/interface/IIncomeRepository.ts
@@ -0,0 +1,12 @@
+import { Income } from "@prisma/client";
+import { IIncomeData } from "../../interfaces/IIncomeData";
+
+interface IIncomeRepository {
+  create(data: IIncomeData): Promise<Income>;
+  update(id: number, userId: number, data: IIncomeData): Promise<Income>;
+  findById(id: number, userId: number): Promise<Income | null>;
+  list(userId: number): Promise<Income[]>;
+  delete(id: number, userId: number): Promise<void>;
+}
+
+export { IIncomeRepository };

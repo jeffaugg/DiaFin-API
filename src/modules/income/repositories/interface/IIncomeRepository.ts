1373afd8fa2fc304f8d2d6e76bb4e66c4b429593
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-17 23:50:09 -0300

    feat: adiciona userId ao método create da interface IIncomeRepository

--- src/modules/income/repositories/interface/IIncomeRepository.ts
+++ src/modules/income/repositories/interface/IIncomeRepository.ts
@@ -2,7 +2,11 @@ import { Income } from "@prisma/client";
 import { IIncomeData } from "../../interfaces/IIncomeData";
 
 interface IIncomeRepository {
-  create(data: IIncomeData): Promise<Income>;
+  create(
+    data: IIncomeData & {
+      userId: number;
+    },
+  ): Promise<Income>;
   update(id: number, userId: number, data: IIncomeData): Promise<Income>;
   findById(id: number, userId: number): Promise<Income | null>;
   list(userId: number): Promise<Income[]>;
